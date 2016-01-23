972a19b256cae699d12e8e50abc3e2c5cabe9b86
laperry1 <laperry7706@gmail.com>
2016-01-23 17:37:43 -0500

    Update toolchain
    Change-Id: I6c0ea107b8c74aa7d284156bfc69ed1b181b957b

--- envsetup.sh
+++ envsetup.sh
@@ -189,7 +189,7 @@ function setpaths()
     case $ARCH in
         arm)
             # Legacy toolchain configuration used for ARM kernel compilation
-            toolchaindir=arm/arm-eabi-5.3/bin
+            toolchaindir=arm/arm-eabi-6.0/bin
             if [ -d "$gccprebuiltdir/$toolchaindir" ]; then
                  export ARM_EABI_TOOLCHAIN="$gccprebuiltdir/$toolchaindir"
                  ANDROID_KERNEL_TOOLCHAIN_PATH="$ARM_EABI_TOOLCHAIN":
