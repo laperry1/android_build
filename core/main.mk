1789b7075198cf8303b31755733226234e592aec
laperry1 <laperry7706@gmail.com>
2016-02-07 02:43:42 -0500

    Fix revert derp
    Change-Id: I82fa2b4ef33752cf0c643ace72e75f317221e58b

--- core/main.mk
+++ core/main.mk
@@ -344,13 +344,17 @@ ifneq (,$(user_variant))
     endif
   endif
 
-# Target is more debuggable and adbd is on by default
-ADDITIONAL_DEFAULT_PROPERTIES += ro.debuggable=1
-# Enable Dalvik lock contention logging.
-ADDITIONAL_BUILD_PROPERTIES += 
-dalvik.vm.lockprof.threshold=500
-# Include the debugging/testing OTA keys in this build.
-INCLUDE_TEST_OTA_KEYS := true
+  # Disallow mock locations by default for user builds
+  ADDITIONAL_DEFAULT_PROPERTIES += ro.allow.mock.location=0
+
+else # !user_variant
+  # Turn on checkjni for non-user builds.
+  ADDITIONAL_BUILD_PROPERTIES += ro.kernel.android.checkjni=1
+  # Set device insecure for non-user builds.
+  ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=0
+  # Allow mock locations by default for non user builds
+  ADDITIONAL_DEFAULT_PROPERTIES += ro.allow.mock.location=1
+endif # !user_variant
 
 ifeq (true,$(strip $(enable_target_debugging)))
   # Target is more debuggable and adbd is on by default
