848e5d07dc70b2b8e8c65665b391c4cee38d22d7
laperry1 <laperry7706@gmail.com>
2016-02-07 01:16:25 -0500

    Revert "Less debug on user builds"
    This reverts commit 1279a5ee1c15c908bb5d66c4c5d4cb8075cdf33e.
    
    Change-Id: Ie6d3f9e0d8bf8011861d1629a87bc7d849d30ec8

--- core/main.mk
+++ core/main.mk
@@ -344,17 +344,13 @@ ifneq (,$(user_variant))
     endif
   endif
 
-  # Disallow mock locations by default for user builds
-  ADDITIONAL_DEFAULT_PROPERTIES += ro.allow.mock.location=0
-
-else # !user_variant
-  # Turn on checkjni for non-user builds.
-  ADDITIONAL_BUILD_PROPERTIES += ro.kernel.android.checkjni=1
-  # Set device insecure for non-user builds.
-  ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=0
-  # Allow mock locations by default for non user builds
-  ADDITIONAL_DEFAULT_PROPERTIES += ro.allow.mock.location=1
-endif # !user_variant
+# Target is more debuggable and adbd is on by default
+ADDITIONAL_DEFAULT_PROPERTIES += ro.debuggable=1
+# Enable Dalvik lock contention logging.
+ADDITIONAL_BUILD_PROPERTIES += 
+dalvik.vm.lockprof.threshold=500
+# Include the debugging/testing OTA keys in this build.
+INCLUDE_TEST_OTA_KEYS := true
 
 ifeq (true,$(strip $(enable_target_debugging)))
   # Target is more debuggable and adbd is on by default
